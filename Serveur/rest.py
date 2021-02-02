6bb87e4888c85b5926894709611d8a2f3d22a963
lephoquebleu <younes.chefou@gmail.com>
2021-02-02 22:45:11 +0100

    Ajout de qqes fonctions

--- Serveur/rest.py
+++ Serveur/rest.py
@@ -2,6 +2,7 @@
 # curl -X POST http://localhost:8888/Logement/\?Nom\=Cionaire\&Prenom\=Dick\&idAd\=2
 
 import http.server, urllib.parse, sqlite3, requests, threading,  socketserver
+import json
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
 	def __init__(self, *args, **kwargs):
@@ -12,9 +13,12 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
 	def do_GET(self):
 		"""Respond to a GET request."""
 		res = urllib.parse.urlparse(self.path)
+		#TODO : Remplacer par les noms de tables
+		tables = ['/utilisateurs', '/logement', '/pieces', '/references', '/plantes_utilisateur']
+
 		if self.path.lower() == "/favicon.ico" : #Icone dans la barre d'accueil
 			return
-		elif self.path.lower() == "/accueil": #Page d'accueil, accueil comme
+		elif self.path.lower() == "/" or self.path.lower() == "/accueil": #Page d'accueil, accueil comme
 			self.send_response(200)
 			self.send_header("Content-type", "text/html")
 			self.end_headers()
@@ -54,21 +58,102 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
 			codeHTML += f.read()
 			self.wfile.write(bytes(str(codeHTML), 'UTF-8'))
 
-		elif self.path.lower() == "/mesplantes"
-
-		#TODO : Remplacer par une requete recuperant les noms de tables
-		tables = ['/utilisateurs', '/logement', '/pieces', '/references', '/plantes_utilisateur']
-		elif self.path.lower() in tables: #Selection de table dans la BDD
+		elif self.path.lower() == "/mesplantes":
 			self.send_response(200)
 			self.send_header("Content-type", "text/html")
 			self.end_headers()
-			rep = self.mysql.select(res.path)
-			self.wfile.write(bytes(str(rep)+'\n', 'UTF-8'))
 
-	else:
-		self.send_response(404)
-		self.send_header("Content-type", "text/html")
-		self.end_headers()
+			codeHTML = """
+			<!DOCTYPE html>
+			<html lang="fr">
+			    <head>
+
+			        <title>Mes plantes</title>
+					<meta charset="utf-8">
+			        <meta name="viewport" content="width=device-width, initial-scale=1">
+			        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+			        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+			        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+			        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+			"""
+
+			f = open("barre_navigation.html")
+			codeHTML += f.read()
+			codeHTML += self.mesplantes() #TODO: A voir si des parametres sont necessaires, normalement non
+			self.wfile.write(bytes(str(codeHTML), 'UTF-8'))
+
+		elif self.path.lower() == "/guide_plante":
+			pass
+
+		elif self.path.lower() == "/configuration":
+			elf.send_response(200)
+			self.send_header("Content-type", "text/html")
+			self.end_headers()
+
+			codeHTML = """
+			<!DOCTYPE html>
+			<html lang="fr">
+				<head>
+
+					<title>Configuration</title>
+					<meta charset="utf-8">
+					<meta name="viewport" content="width=device-width, initial-scale=1">
+					<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+					<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+					<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+					<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+					<style>
+					#div_img{
+						position:fixed;
+						padding:0;
+						margin:0;
+						top:0;
+						left:0;
+						width:100%;
+						height:100%;
+						background-image: url('https://elemental.green/wp-content/uploads/2017/06/Depositphotos_10522904_m-2015-999x640.jpg');
+						background-size: cover;
+						z-index: -1;
+						opacity:0.4;
+					}
+					</style>
+				</head>
+			"""
+			f = open("barre_navigation.html")
+			codeHTML += f.read()
+			codeHTML += self.pageConfiguration()
+			f = open("fin_page.html")
+			codeHTML += f.read()
+			self.wfile.write(bytes(str(codeHTML), 'UTF-8'))
+
+
+		elif self.path.lower() in tables: #Selection de table dans la BDD
+			#Ici, renvoyer un fichier de type JSON à l'utilisateur
+			#Exemple :
+			# 0 : {
+			#	'nom':'plante'
+			#	'espece': 'cactus'
+			#}
+			# 1 : {
+			#	'nom':'fleur'
+			#	'espece': 'rose'
+			#}
+			elem = path.split('/')
+			if len(elem) > 1:
+				self.send_response(200)
+				self.send_header("Content-type", "text/html")
+				self.end_headers()
+				rep = self.mysql.toJSON(elem[1])
+				self.wfile.write(bytes(str(rep)+'\n', 'UTF-8'))
+			else:
+				self.send_response(404)
+				self.send_header("Content-type", "text/html")
+				self.end_headers()
+
+		else:
+			self.send_response(404)
+			self.send_header("Content-type", "text/html")
+			self.end_headers()
 
 	def do_POST(self):
 		"""Respond to a POST request."""
@@ -87,62 +172,6 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
 		self.send_response(200)
 		self.send_header("Content-type", "text/html")
 		self.end_headers()
-
-	def pieChart(self, rep):
-		codeHTML = """
-		<html>
-		  <head>
-			<!--Load the AJAX API-->
-			<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-			<script type="text/javascript">
-
-			  // Load the Visualization API and the corechart package.
-			  google.charts.load('current', {'packages':['corechart']});
-
-			  // Set a callback to run when the Google Visualization API is loaded.
-			  google.charts.setOnLoadCallback(drawChart);
-
-			  // Callback that creates and populates a data table,
-			  // instantiates the pie chart, passes in the data and
-			  // draws it.
-			  function drawChart() {
-
-				// Create the data table.
-				var data = new google.visualization.DataTable();
-				data.addColumn('string', 'Type de facture');
-				data.addColumn('number', 'Montant à payer');
-				data.addRows([
-		"""
-		data = {'Electricite': 0, 'Internet': 0, 'Eau':0}
-		for rangee in rep:
-			print(rangee)
-			data[rangee[0]] += rangee[3]
-
-		stringData = "['Electricite',"+str(data['Electricite'])+"],"
-		stringData += "['Internet', "+str(data['Internet'])+"],"
-		stringData += "['Eau', "+str(data['Eau'])+"],"
-
-		finHTML = """]);
-				// Set chart options
-				var options = {'title':'Montant à payer par type de facture',
-							   'width':400,
-							   'height':300};
-
-				// Instantiate and draw our chart, passing in some options.
-				var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-				chart.draw(data, options);
-			  }
-			</script>
-		  </head>
-
-		  <body>
-			<!--Div that will hold the pie chart-->
-			<div id="chart_div"></div>
-		  </body>
-		</html>
-		"""
-		codeHTML += stringData + finHTML
-		return codeHTML
 
 	def meteo(self):
 		#Lien vers JSON, renvoyant prévision
@@ -172,213 +201,35 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
 
 		return strForecast
 
-	def pageConso(self, annee):
-		codeHTML = """
-			<!--Load the AJAX API-->
-			<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-			<script type="text/javascript">
+	#Recupere les informations
+	def mesplantes(self):
+		# req = "select * from capteur;"
+		# tabElem = self.mysql.c.execute(req).fetchall()
+		conn = self.mysql.conn
+		c = conn.cursor()
 
-			  // Load the Visualization API and the corechart package.
-			  google.charts.load('current', {'packages':['corechart']});
-
-			  // Set a callback to run when the Google Visualization API is loaded.
-			  google.charts.setOnLoadCallback(drawChart);
-
-			  // On fait quatre
-			  function drawChart() {
-
-				// Create the data table.
-				var dataElec = new google.visualization.DataTable();
-				dataElec.addColumn('string', 'Date');
-				dataElec.addColumn('number', 'Consommation electrique (kWh)');
-
-				var dataGaz = new google.visualization.DataTable();
-				dataGaz.addColumn('string', 'Date');
-				dataGaz.addColumn('number', 'Consommation Gaz (kWh)');
-
-				var dataEau = new google.visualization.DataTable();
-				dataEau.addColumn('string', 'Date');
-				dataEau.addColumn('number', 'Consommation Eau (m3)');
-
+		codeHTML ="""
 		"""
+		#On recupere l'id max pour les chambres existantes
+		req = "SELECT MAX(id) FROM room;"
+		nbChambres = c.execute(req).fetchall()[0][0]
 
-		#On ajoute les rangées pour chaque graphique
+		print("Nombre de chambres :")
+		print(nbChambres)
 
-		req = "select * from facture where datePaiement >= \"%d-01-01\" AND datePaiement < \"%d-01-01\" AND typeFacture = \"Electricite\" " %(annee, annee+1)
-		tabElem = self.mysql.c.execute(req).fetchall()
+		#On boucle maintenant pour recuperer chaque plante et les informations associés
+		for id_room in range (1, nbChambres+1):
 
-		stringData = "dataElec.addRows(["
+			req = "SELECT * FROM PLANTE WHERE RoomReference="+str(id_room)+";"
+			donnees = self.toJSON('PLANTE', req).json()
 
-		for rangee in tabElem:
-			#print(rangee)
-			stringData += "[\'"+str(rangee[2])+"\', "+str(rangee[4])+"],\n"
+			
 
-		stringData += "]);\n"
-
-		req = "select * from facture where datePaiement >= \"%d-01-01\" AND datePaiement < \"%d-01-01\" AND typeFacture = \"Gaz\" " %(annee, annee+1)
-		tabElem = self.mysql.c.execute(req).fetchall()
-
-		stringData += "dataGaz.addRows(["
-
-		for rangee in tabElem:
-			#print(rangee)
-			stringData += "['"+str(rangee[2])+"\', "+str(rangee[4])+"],\n"
-
-		stringData += "]);\n"
-
-		req = "select * from facture where datePaiement >= \"%d-01-01\" AND datePaiement < \"%d-01-01\" AND typeFacture = \"Eau\" " %(annee, annee+1)
-		tabElem = self.mysql.c.execute(req).fetchall()
-
-		stringData += "dataEau.addRows(["
-
-		for rangee in tabElem:
-			#print(rangee)
-			stringData += "['"+str(rangee[2])+"\', "+str(rangee[4])+"],\n"
-
-		stringData += "]);\n"
-
-		finHTML = """
-				// Set chart options
-				var optionsElec = {
-							   title :'Consommation electricite en """+str(annee)+"""',
-          					   legend: { position: 'bottom' },
-							   is3D : true
-							   };
-
-				// Instantiate and draw our chart, passing in some options.
-				var chartElec = new google.visualization.LineChart(document.getElementById('curve_chart_elec'));
-				chartElec.draw(dataElec, optionsElec);
-
-				var optionsGaz = {
-							   title :'Consommation Gaz en """+str(annee)+"""',
-          					   legend: { position: 'bottom' },
-							   is3D : true
-							   };
-
-				// Instantiate and draw our chart, passing in some options.
-				var chartGaz = new google.visualization.LineChart(document.getElementById('curve_chart_gaz'));
-				chartGaz.draw(dataGaz, optionsGaz);
-
-				var optionsEau = {
-								title:'Consommation Eau en """+str(annee)+"""',
-				 				legend: { position: 'bottom' },
-								is3D : true
-								};
-
-				// Instantiate and draw our chart, passing in some options.
-				var chartEau = new google.visualization.LineChart(document.getElementById('curve_chart_eau'));
-				chartEau.draw(dataEau, optionsEau);
-			  }
-
-			  $(window).resize(function(){
-			  	drawChart();
-			  });
-			</script>
-		  </head>
-		"""
-		codeHTML += stringData + finHTML
+		# f = open("dash.html")
+		# codeHTML = f.read()
 		return codeHTML
 
-	def pageCapteurs(self):
 
-		req = "select * from capteur;"
-		tabElem = self.mysql.c.execute(req).fetchall()
-
-		codeHTML = """
-		<div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-		   <h1 class="display-4">Statut des capteurs</h1>
-		</div>
-		"""
-
-		codeHTML += '<div class="container">\n'
-		codeHTML += '<div class="card-deck mb-3 text-center">\n'
-
-		for rangee in tabElem:
-			nom = rangee[2]
-			statut = rangee[4]
-
-			codeHTML +='<div class="card mb-4 box-shadow" style="min-width: 200px;">\n'
-			codeHTML += '<div class="card-header">\n'
-			codeHTML += '<h5 class="my-0 font-weight-normal text-nowrap">'+nom+'</h5>\n'
-			codeHTML += '</div>\n'
-			codeHTML += '<div class="card-body">\n'
-			codeHTML += '<h4 class="card-title pricing-card-title titreStatut">'
-			if(statut == 1):
-				codeHTML += "Actif </h4>\n"
-			else:
-				codeHTML += "Inactif </h4>\n"
-
-            # <button type="button" class="btn btn-lg btn-block btn-primary">Get started</button>
-			codeHTML += '</div>\n'
-			codeHTML += '</div>\n'
-
-		codeHTML += '</div>\n'
-		codeHTML += '</div>\n'
-		return codeHTML;
-
-	def pageEconomie(self):
-
-		req = 'select sum(montant) from facture where datePaiement >= "2020-01-01" and typeFacture="Electricite";'
-		consoElec2020 = self.mysql.c.execute(req).fetchall()[0][0]
-		req = 'select sum(montant) from facture where datePaiement < "2020-01-01" and typeFacture="Electricite";'
-		consoElec2019 = self.mysql.c.execute(req).fetchall()[0][0]
-
-		req = 'select sum(montant) from facture where datePaiement >= "2020-01-01" and typeFacture="Gaz";'
-		consoGaz2020 = self.mysql.c.execute(req).fetchall()[0][0]
-		req = 'select sum(montant) from facture where datePaiement < "2020-01-01" and typeFacture="Gaz";'
-		consoGaz2019 = self.mysql.c.execute(req).fetchall()[0][0]
-
-		req = 'select sum(montant) from facture where datePaiement >= "2020-01-01" and typeFacture="Eau";'
-		consoEau2020 = self.mysql.c.execute(req).fetchall()[0][0]
-		req = 'select sum(montant) from facture where datePaiement < "2020-01-01" and typeFacture="Eau";'
-		consoEau2019 = self.mysql.c.execute(req).fetchall()[0][0]
-
-		progElec = 100 - (consoElec2020/consoElec2019 * 100);
-		progGaz = 100 - (consoGaz2020/consoGaz2019 * 100);
-		progEau = 100 - (consoEau2020/consoEau2019 * 100);
-
-		codeHTML = """
-		<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-	    <script type="text/javascript">
-	      google.charts.load('current', {'packages':['bar']});
-	      google.charts.setOnLoadCallback(drawChart);
-
-	      function drawChart() {
-	        var data = google.visualization.arrayToDataTable([
-	          ['Type de facture', '2019', '2020'],
-		"""
-		codeHTML += "['Electricite',"+str(consoElec2019)+","+str(consoElec2020)+"],"
-		codeHTML += "['Gaz',"+str(consoGaz2019)+","+str(consoGaz2020)+"],"
-		codeHTML += "['Eau',"+str(consoEau2019)+","+str(consoEau2020)+"],"
-		codeHTML += """
-	        ]);
-
-	        var options = {
-	          chart: {
-	            title: 'Economies réalisées',
-	            subtitle: 'Comparaison factures payées : 2019-2020',
-	          }
-	        };
-
-	        var chart = new google.charts.Bar(document.getElementById('column_chart'));
-
-	        chart.draw(data, google.charts.Bar.convertOptions(options));
-	      }
-
-		  $(window).resize(function(){
-		  	drawChart();
-		  });
-
-		  $(window). load(function commentaire(){
-		  	$("#commentaire").append('<li>Economies éléctricité : """+"{:.2f}".format(progElec)+"""%</li>');
-			$("#commentaire").append('<li>Economies gaz : """+"{:.2f}".format(progGaz)+"""%</li>');
-			$("#commentaire").append('<li>Economies eau : """+"{:.2f}".format(progEau)+"""%</li>');
-		  });
-	    </script>
-		</head>
-		"""
-
-		return codeHTML
 
 	#Affiche les informations sur le logement et permet d'ajouter des capteurs
 	def pageConfiguration(self):
@@ -462,6 +313,35 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
 		"""
 		return codeHTML;
 
+	def toJSON(self, table, req="SELECT * FROM "+table+";"):
+		reqColumn = "PRAGMA table_info("+table+");"
+
+		conn = self.mysql.conn
+		c = conn.cursor()
+		columns = c.execute(reqColumn).fetchall()
+		donnees = c.execute(req).fetchall()
+
+		fichierJSON = """
+		[
+		"""
+		cpt = 0;
+		for rangee in donnees:
+		    print(rangee)
+		    fichierJSON += '\t\"'+str(cpt)+'\": {\n'
+		    for i in range (0, len(columns)):
+		        fichierJSON += '\t\"'+str(columns[i][1])+'\":\"'+str(rangee[i])+'\",'
+		        fichierJSON += '\n'
+		    fichierJSON += '\t}'
+		    fichierJSON += '\n\n'
+		    cpt += 1
+
+		fichierJSON += """
+		]
+		"""
+
+		return fichierJSON
+
+
 class MySQL():
 	def __init__(self, name):
 		self.c = None
@@ -497,6 +377,7 @@ def serve_on_port(port) :
 	server = ThreadingHTTPServer (("localhost", port) , MyHandler)
 	server.serve_forever()
 
+#TODO : modifier cette fonction une fois les capteurs ajoutés
 #Recupere les ports de communication des capteurs et demarre les threads associés
 def start_capteur_threads():
 	base = MySQL('logement.db')
@@ -508,7 +389,7 @@ def start_capteur_threads():
 
 
 if __name__ == '__main__':
-	start_capteur_threads()
+	# start_capteur_threads()
 	#On ouvre un 4e thread pour toutes les autres requêtes
 	threading.Thread(target=serve_on_port, args=[8888]).start()
 	print("Divers : 8888");
